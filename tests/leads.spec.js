e02cbcd7e28b343dbcc3399798b6606026adcd96
Rodrigo Souza <rodrigosouza@Laptop-de-Rodrigo.local>
2026-01-31 20:11:57 -0300

    Playwright - automating an alert message

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+test('Positive Test Scenario', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   //await page.click('//button[text()="Aperte o play... se tiver coragem"]') técnica de xpath
@@ -33,3 +33,23 @@ test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
 
 });
+
+test('Negative Test Scenario', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]') técnica de xpath
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click() //getByRole é mais comum no playwright.
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Rodrigo Souza')
+  await page.getByPlaceholder('Seu email principal').fill('rod.com.br')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+  
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+})
