f57cde6f522a4968892d6616995231158942e9ab
Rodrigo Souza <rodrigosouza@Laptop-de-Rodrigo.local>
2026-02-03 18:19:29 -0300

    Checking empty fields with Playwright framework

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -53,3 +53,58 @@ test('Negative Test Scenario', async ({ page }) => {
   
   await expect(page.locator('.alert')).toHaveText('Email incorreto')
 })
+
+test('Negative Test - name field empty', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]') técnica de xpath
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click() //getByRole é mais comum no playwright.
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu email principal').fill('rod@gmail.com')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+  
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('Negative Test - email empty', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]') técnica de xpath
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click() //getByRole é mais comum no playwright.
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Rodrigo Souza')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+  
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('Negative Test - all fields empty', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  //await page.click('//button[text()="Aperte o play... se tiver coragem"]') técnica de xpath
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click() //getByRole é mais comum no playwright.
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+  
+  await expect(page.locator('.alert')).toHaveText(['Campo obrigatório', 'Campo obrigatório'])
+})
