022bef1d70959aa3838833db657efb580d75b58a
rodsouza83 <rodrigocarlos1883@gmai.com>
2026-02-20 16:57:31 -0300

    Creating a LoginPage Automation

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -20,10 +20,35 @@ test('should log in successfully', async ({ page }) => {
 
 })
 
-test('using incorrect user and password', async ({ page }) => {
+test('trying to validate using an invalid email', async ({ page }) => {
     await loginPage.visit()
-    await loginPage.submit('admin@zombie.com', 'pwd1234')
+    await loginPage.submit('www.rodrigo.com.br', 'pwd1234')
     
-    const message = 'Oops!Ocorreu um erro ao tentar efetuar o login. Por favor, verifique suas credenciais e tente novamente.'
-    await toast.haveText(message)
+    await loginPage.alertHaveText('Email incorreto')
 })
+
+test('trying to validate using an empty email field', async ({ page }) => {
+    await loginPage.visit()
+    await loginPage.submit('', 'pwd1234')
+    
+    await loginPage.alertHaveText('Campo obrigatório')
+})
+
+test('trying to validate using empty password field', async ({ page }) => {
+    await loginPage.visit()
+    await loginPage.submit('rods83@gmail.com', '')
+    
+    await loginPage.alertHaveText('Campo obrigatório')
+})
+
+
+test('trying to validate using empty  email and password field', async ({ page }) => {
+    await loginPage.visit()
+    await loginPage.submit('', '')
+    
+    await loginPage.alertHaveText(['Campo obrigatório', 'Campo obrigatório'])
+})
+
+
+
+
