70d4bdc7907ea0a8d3d42598a19877c510303130
rodsouza83 <rodrigocarlos1883@gmai.com>
2026-03-16 19:45:16 -0300

    Using duplicated email

--- tests/e2e/leadsPageObj.spec.js
+++ tests/e2e/leadsPageObj.spec.js
@@ -1,37 +1,54 @@
-// @ts-check
-import { test, expect } from '@playwright/test'
-
+const { test, expect } = require('@playwright/test')
 const { faker } = require('@faker-js/faker')
 
-const leadsName = faker.person.fullName()
-const leadsEmail = faker.internet.email()
-
 const { LandingPage } = require('../pages/LandingPage')
-
 const { Toast } = require('../pages/Components')
 
-let landingPage
-let toast
+let leadsName
+let leadsEmail
 
-
-test.beforeEach(async ({ page }) => {
-  landingPage = new LandingPage(page)
-  toast = new Toast(page)
+test.beforeEach(async () => {
+  leadsName = faker.person.fullName()
+  leadsEmail = faker.internet.email()
 })
 
 test('Positive Test Scenario', async ({ page }) => {
   const landingPage = new LandingPage(page)
+  const toast = new Toast(page)
 
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm(leadsName, leadsEmail)
-  const message = ('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!')
+
+  const message =
+    'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+
   await toast.haveText(message)
+})
 
+test('Negative Test Scenario - using the same email', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  const toast = new Toast(page)
 
-});
+  const successMessage =
+    'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+
+  const duplicateMessage =
+    'O endereço de e-mail fornecido já está registrado em nossa fila de espera.'
+
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm(leadsName, leadsEmail)
+  await toast.haveText(successMessage)
+
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Outro Nome', leadsEmail)
+  await toast.haveText(duplicateMessage)
+})
 
 test('Negative Test Scenario', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('Rodrigo Souza', 'rodsz.mail.com')
@@ -40,6 +57,8 @@ test('Negative Test Scenario', async ({ page }) => {
 })
 
 test('Negative Test - name field empty', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('', 'rodsz@gmail.com')
@@ -48,6 +67,8 @@ test('Negative Test - name field empty', async ({ page }) => {
 })
 
 test('Negative Test - email empty', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('Rodrigo Souza', '')
@@ -56,6 +77,8 @@ test('Negative Test - email empty', async ({ page }) => {
 })
 
 test('Negative Test - all fields empty', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('', '')
