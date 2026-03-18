3fa4a9a5193f742c508bcb234ee4b2bb08dbe01e
rodsouza83 <rodrigocarlos1883@gmai.com>
2026-03-18 15:35:40 -0300

    Creating a login page in Zombie+ movies

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,6 +1,25 @@
 const { test } = require('@playwright/test')
 
-test('should be register a new movie', async ({ page }) => {
+const { LoginPage } = require('../pages/LoginPage')
 
+const { MoviesPage } = require('../pages/MoviesPages')
+
+const { Toast } = require('../pages/Components')
+
+let loginPage
+let moviesPage
+let toast
+
+test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page)
+    moviesPage = new MoviesPage(page)
+    toast = new Toast(page)
+
+})
+
+test('should be register a new movie', async ({ page }) => {
+    await loginPage.visit()
+    await loginPage.submit('admin@zombieplus.com', 'pwd123')
+    await moviesPage.isLoggedIn()
 
 })
