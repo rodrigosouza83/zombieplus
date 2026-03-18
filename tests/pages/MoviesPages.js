d2ad48d60c5839240ba1d6ac98f8ab142ddb69f6
rodsouza83 <rodrigocarlos1883@gmai.com>
2026-03-18 19:00:33 -0300

    Implementing React Select

--- tests/pages/MoviesPages.js
+++ tests/pages/MoviesPages.js
@@ -4,19 +4,25 @@ export class MoviesPage {
     constructor(page) {
         this.page = page
     }
-    
+
     async isLoggedIn() {
         await this.page.waitForLoadState('networkidle')
         await expect(this.page).toHaveURL(/.*admin/)
     }
 
-    async create(title, overview, company, release_year){
+    async create(title, overview, company, release_year) {
 
         await this.page.locator('a[href$="register"]').click()
 
         await this.page.getByLabel('Titulo do filme').fill(title)
         await this.page.getByLabel('Sinopse').fill(overview)
 
+        await this.page.locator('#select_company_id .react-select__indicator').click()
+        await this.page.locator('.react-select__option').filter({ hasText: company }).click()
+        
+        await this.page.locator('#select_year .react-select__indicator').click()
+        await this.page.locator('.react-select__option').filter({ hasText: release_year }).click()
+        
 
     }
 }
